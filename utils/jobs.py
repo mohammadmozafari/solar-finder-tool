1a0ee32005d9cf6d5a5a3c01415ce3d58904f45c
srmt99 <soroushmortazavi99@gmail.com>
2024-01-03 12:31:01 +0000

    fixed python path

--- utils/jobs.py
+++ utils/jobs.py
@@ -6,6 +6,6 @@ def start_job(s_lat, s_long, t_lat, t_long, exp_name):
     print(s_lat, s_long, t_lat, t_long, exp_name)
     os.system(
         (f'cd {config.PROJECT_ROOT_PATH} && '
-         f'python scripts/scan.py -s1 {s_lat} -s2 {s_long} -d1 {t_lat} -d2 {t_long} '
+         f'{config.PYTHON_PATH} scripts/scan.py -s1 {s_lat} -s2 {s_long} -d1 {t_lat} -d2 {t_long} '
          f'-b 64 -n {exp_name.replace(" ", "_")}')
     )
